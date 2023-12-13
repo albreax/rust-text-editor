93ac61849e95deb735f477e4df01e4ed1fb45f48
Albrecht Naumann <albrecht.naumann@posteo.de>
2023-12-13 22:05:24 +0100

    Clear Terminal implemented

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Rust Test Editor
 
-It's just me learning Rust by writing a text editor.
+It's just me learning Rust by writing a CLI text editor.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
+use crossterm::terminal::enable_raw_mode;
+use tui::{terminal::Terminal, backend::CrosstermBackend};
+
 fn main() {
-    println!("Hello, world!");
+  enable_raw_mode().expect("can run in raw mode");
+  let backend = CrosstermBackend::new(io::stdout());
+  let mut terminal = match Terminal::new(backend) {
+    Ok(terminal) => terminal,
+    Err(err) => {
+      eprintln!("Failed to create terminal: {}", err);
+      return;
+    }
+  };
+  terminal.clear().expect("failed to clear terminal");
 }
